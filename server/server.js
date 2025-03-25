321f53a2ae61022601d00ec69ace88992284f122
nisal <nhfernando890@gmail.com>
2025-03-25 15:28:28 +0530

    menu page created

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const menuRoutes = require("./routes/menuRoutes"); // Import routes
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
+
+app.use("/api/menu", menuRoutes); // This will handle CRUD operations for menu
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
